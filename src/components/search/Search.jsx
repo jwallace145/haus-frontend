4821869518942e3332e32876505c5946367931de
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-08 14:20:25 -0500

    started search page

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+
+export default class Search extends Component {
+  render() {
+    return (
+      <>
+        <Container>
+          <h1>Search</h1>
+        </Container>
+        <Container>
+          <h1>Songs</h1>
+        </Container>
+      </>
+    );
+  }
+}
