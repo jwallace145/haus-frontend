7200fe63d7b6d756bdb25c559ea4cce383299a4d
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-08 13:54:03 -0500

    idk why it doesnt track the app.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,36 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Logout from "./components/logout/Logout";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path='/songintake' component={SongIntake} />
-        <Route path='/login' component={Login} />
-        <Route path='/profile/:username' component={Profile} />
-        <Route path='/home' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path="/register" component={Register} />
-        <Route path='/logout/:username' component={Logout} />
-      </Switch>
-    </Router>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: "bob"
+    }
+  }
+
+  callBack = (data) => {
+    console.log('data: ' + data);
+    this.setState({username: data});
+  }
+
+  render() {
+    return (
+      <Router>
+        <Header username={this.state.username} />
+        <Switch>
+          <Route path='/songintake' component={SongIntake} />
+          <Route path='/login' component={() => <Login appCallBack={this.callBack}/>} />
+          <Route path='/profile/:username' component={Profile} />
+          <Route path='/home' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path="/register" component={Register} />
+          <Route path='/logout/:username' component={Logout} />
+        </Switch>
+      </Router>
+    );
+  }
 }
-
-export default App;
