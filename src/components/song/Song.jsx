dec48f24f996eac84776bb1929de4a34b9b73edb
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-07 23:58:28 -0500

    added login backend logic

--- src/components/song/Song.jsx
+++ src/components/song/Song.jsx
@@ -3,10 +3,6 @@ import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Song extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <Card bg="light">
