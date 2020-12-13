4f164dae7c401da59bf73882e9a3b6052a19b748
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-12 19:56:57 -0500

    refactored song intake to be a functional component

--- src/components/songintake/SongIntake.jsx
+++ src/components/songintake/SongIntake.jsx
@@ -1,31 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
-export default class SongIntake extends Component {
-  constructor(props) {
-    super(props);
+export default function SongIntake(props) {
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [album, setAlbum] = useState("");
+  const [cover, setCover] = useState("");
 
-    this.state = {
-      id: "",
-      title: "",
-      artist: "",
-      album: "",
-      cover: "",
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
+  function handleClick(e) {
     e.preventDefault();
 
     let formData = new FormData();
-    formData.append("title", this.state.title);
-    formData.append("artist", this.state.artist);
-    formData.append("album", this.state.album);
-    formData.append("cover", this.state.cover);
+    formData.append("title", title);
+    formData.append("artist", artist);
+    formData.append("album", album);
+    formData.append("cover", cover);
 
     const requestOptions = {
       method: "POST",
@@ -39,47 +30,45 @@ export default class SongIntake extends Component {
       });
   }
 
-  render() {
-    return (
-      <>
-        <Container>
-          <Form>
-            <Form.Group controlId="title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="title"
-                placeholder="enter title"
-                onChange={(e) => this.setState({ title: e.target.value })}
-              />
-            </Form.Group>
-            <Form.Group controlId="artist">
-              <Form.Label>Artist</Form.Label>
-              <Form.Control
-                type="artist"
-                placeholder="enter artist"
-                onChange={(e) => this.setState({ artist: e.target.value })}
-              />
-            </Form.Group>
-            <Form.Group controlId="album">
-              <Form.Label>Album</Form.Label>
-              <Form.Control
-                type="album"
-                placeholder="enter album"
-                onChange={(e) => this.setState({ album: e.target.value })}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.File
-                label="Album Cover"
-                onChange={(e) => this.setState({ cover: e.target.files[0] })}
-              ></Form.File>
-            </Form.Group>
-            <Button variant="dark" onClick={this.handleClick}>
-              Submit
-            </Button>
-          </Form>
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <Container>
+        <Form>
+          <Form.Group controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="title"
+              placeholder="enter title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="artist">
+            <Form.Label>Artist</Form.Label>
+            <Form.Control
+              type="artist"
+              placeholder="enter artist"
+              onChange={(e) => setArtist(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="album">
+            <Form.Label>Album</Form.Label>
+            <Form.Control
+              type="album"
+              placeholder="enter album"
+              onChange={(e) => setAlbum(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.File
+              label="Album Cover"
+              onChange={(e) => setCover(e.target.files[0])}
+            ></Form.File>
+          </Form.Group>
+          <Button variant="dark" onClick={handleClick}>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
 }
