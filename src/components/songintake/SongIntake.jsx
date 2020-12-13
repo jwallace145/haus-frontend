7ae766457aa1850fa26f48c9f7e100b6062a95b4
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-12 19:26:48 -0500

    refactored song intake component

--- src/components/songintake/SongIntake.jsx
+++ src/components/songintake/SongIntake.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 export default class SongIntake extends Component {
   constructor(props) {
@@ -9,108 +10,75 @@ export default class SongIntake extends Component {
     this.state = {
       id: "",
       title: "",
-      album: "",
       artist: "",
-      songfile: "",
-      songfilename: "",
+      album: "",
+      cover: "",
     };
 
     this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    if (event.target.id === "title") {
-      this.setState({ title: event.target.value });
-    } else if (event.target.id === "album") {
-      this.setState({ album: event.target.value });
-    } else if (event.target.id === "artist") {
-      this.setState({ artist: event.target.value });
-    }
   }
 
   handleClick(e) {
     e.preventDefault();
 
+    let formData = new FormData();
+    formData.append("title", this.state.title);
+    formData.append("artist", this.state.artist);
+    formData.append("album", this.state.album);
+    formData.append("cover", this.state.cover);
+
     const requestOptions = {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: this.state.title,
-        artist: this.state.artist,
-        album: this.state.album,
-      }),
+      body: formData,
     };
 
     fetch("http://127.0.0.1:5000/songs?username=bob", requestOptions)
       .then((response) => response.json())
       .then((song) => {
-        console.log("song id: " + song["song"]["id"]);
-        this.setState({ id: song["song"]["id"] });
-
-        let formData = new FormData();
-
-        formData.append("song_cover", this.state.songfile);
-        formData.append("song_id", this.state.id);
-
-        let options = {
-          method: "POST",
-          body: formData,
-        };
-        fetch("http://127.0.0.1:5000/songs/upload-song-cover", options)
-          .then((response) => response.json())
-          .then((data) => console.log(data));
+        console.log(song);
       });
   }
 
   render() {
     return (
       <>
-        <div className="container">
+        <Container>
           <Form>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
               <Form.Control
                 type="title"
                 placeholder="enter title"
-                onChange={this.handleChange}
+                onChange={(e) => this.setState({ title: e.target.value })}
               />
-              <Form.Text className="text-muted">Enter name of song</Form.Text>
-            </Form.Group>
-            <Form.Group controlId="album">
-              <Form.Label>Album</Form.Label>
-              <Form.Control
-                type="album"
-                placeholder="enter album"
-                onChange={this.handleChange}
-              />
-              <Form.Text className="text-muted">Enter name of album</Form.Text>
             </Form.Group>
             <Form.Group controlId="artist">
               <Form.Label>Artist</Form.Label>
               <Form.Control
                 type="artist"
                 placeholder="enter artist"
-                onChange={this.handleChange}
+                onChange={(e) => this.setState({ artist: e.target.value })}
               />
-              <Form.Text className="text-muted">Enter name of artist</Form.Text>
+            </Form.Group>
+            <Form.Group controlId="album">
+              <Form.Label>Album</Form.Label>
+              <Form.Control
+                type="album"
+                placeholder="enter album"
+                onChange={(e) => this.setState({ album: e.target.value })}
+              />
             </Form.Group>
             <Form.Group>
               <Form.File
                 label="Album Cover"
-                onChange={(e) =>
-                  this.setState({
-                    songfile: e.target.files[0],
-                    songfilename: e.target.files[0].name,
-                  })
-                }
+                onChange={(e) => this.setState({ cover: e.target.files[0] })}
               ></Form.File>
             </Form.Group>
             <Button variant="dark" onClick={this.handleClick}>
               Submit
             </Button>
           </Form>
-        </div>
+        </Container>
       </>
     );
   }
