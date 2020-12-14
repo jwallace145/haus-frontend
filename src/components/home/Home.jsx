aa0d9e69c903ab3ed996158c1b957eb59d2677d0
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-13 21:23:24 -0500

    added pagination on home page and profile page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,11 +5,15 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
+import Button from "react-bootstrap/Button";
+import "./Home.css";
 
 export default function Home(props) {
   const [key, setKey] = useState("recentlyLiked");
   const [recentlyLikedSongs, setRecentlyLikedSongs] = useState([]);
   const [mostLikedSongs, setMostLikedSongs] = useState([]);
+  const [recentlyLikedSongsIndex, setRecentlyLikedSongsIndex] = useState(0);
+  const [mostLikedSongsIndex, setMostLikedSongsIndex] = useState(0);
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/songs")
@@ -25,6 +29,24 @@ export default function Home(props) {
       });
   }, []);
 
+  function handlePageButtonClick(event) {
+    event.preventDefault();
+
+    if (event.target.id === "previous-page") {
+      if (key === "recentlyLiked") {
+        setRecentlyLikedSongsIndex(recentlyLikedSongsIndex - 6);
+      } else if (key === "mostLiked") {
+        setMostLikedSongsIndex(mostLikedSongsIndex - 6);
+      }
+    } else if (event.target.id === "next-page") {
+      if (key === "recentlyLiked") {
+        setRecentlyLikedSongsIndex(recentlyLikedSongsIndex + 6);
+      } else if (key === "mostLiked") {
+        setMostLikedSongsIndex(mostLikedSongsIndex + 6);
+      }
+    }
+  }
+
   return (
     <>
       <Container>
@@ -36,18 +58,23 @@ export default function Home(props) {
             <Container>
               <Row>
                 <Col>
-                  <div className="sidebar">
-                    {recentlyLikedSongs.map((song) => (
-                      <Song
-                        key={song.id}
-                        title={song.title}
-                        artist={song.artist}
-                        album={song.album}
-                        likes={song.likes}
-                        imgsrc={song.cover_url}
-                        created_on={song.created_on}
-                      />
-                    ))}
+                  <div className="sidebar wrapper">
+                    {recentlyLikedSongs
+                      .slice(
+                        recentlyLikedSongsIndex,
+                        recentlyLikedSongsIndex + 6
+                      )
+                      .map((song) => (
+                        <Song
+                          key={song.id}
+                          title={song.title}
+                          artist={song.artist}
+                          album={song.album}
+                          likes={song.likes}
+                          imgsrc={song.cover_url}
+                          created_on={song.created_on}
+                        />
+                      ))}
                   </div>
                 </Col>
               </Row>
@@ -57,24 +84,38 @@ export default function Home(props) {
             <Container>
               <Row>
                 <Col>
-                  <div className="sidebar">
-                    {mostLikedSongs.map((song) => (
-                      <Song
-                        key={song.id}
-                        title={song.title}
-                        artist={song.artist}
-                        album={song.album}
-                        likes={song.count}
-                        imgsrc={song.cover_url}
-                        created_on={song.created_on}
-                      />
-                    ))}
+                  <div className="sidebar wrapper">
+                    {mostLikedSongs
+                      .slice(mostLikedSongsIndex, mostLikedSongsIndex + 6)
+                      .map((song) => (
+                        <Song
+                          key={song.id}
+                          title={song.title}
+                          artist={song.artist}
+                          album={song.album}
+                          likes={song.count}
+                          imgsrc={song.cover_url}
+                          created_on={song.created_on}
+                        />
+                      ))}
                   </div>
                 </Col>
               </Row>
             </Container>
           </Tab>
         </Tabs>
+        <div className="page-buttons">
+          <Button
+            id="previous-page"
+            variant="dark"
+            onClick={handlePageButtonClick}
+          >
+            Previous Page
+          </Button>
+          <Button id="next-page" variant="dark" onClick={handlePageButtonClick}>
+            Next Page
+          </Button>
+        </div>
       </Container>
     </>
   );
