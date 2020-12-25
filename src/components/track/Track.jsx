42052d06f80cf3bf86fa67e9545200832355f1a2
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-12-24 21:05:47 -0500

    initial ratings working

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import Rating from "@material-ui/lab/Rating";
 import "swiper/css/swiper.css";
 
 export default function Track(props) {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${props.track.cover_url})`,
-        width: 200,
-        height: 200,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}
-    ></div>
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${props.track.cover_url})`,
+          width: 200,
+          height: 200,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      ></div>
+      <Rating />
+    </>
   );
 }
